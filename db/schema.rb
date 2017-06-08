ae000bd5be18f1bece13b1e84c00b3e645eacb03
Mohsin Ali <mohammadmohsin@gmail.com>
2017-06-08 13:31:25 +0500

    Coupon model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170607105338) do
+ActiveRecord::Schema.define(version: 20170608082854) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "coupons", force: :cascade do |t|
+    t.string "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "leads", force: :cascade do |t|
     t.string "name"
