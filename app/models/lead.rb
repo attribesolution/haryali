4b1e90fcad0a257d8e124241195077eb6ffff86d
Sarwan Kumar <srladuani@gmail.com>
2017-06-08 16:15:15 +0500

    location model level validations

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -8,7 +8,7 @@ class Lead < ApplicationRecord
   validates :name, :contact, :plant, presence: true, if: :step1?
   validates :quantity, :payment_type,:location, presence: true, if: :step2?
   validates :quantity, numericality: true, if: :step2?
-
+  
   attr_accessor :coupon_code
   attr_accessor :location_type
 
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,4 @@
 class Location < ApplicationRecord
   has_many :leads
+  validates :address, :lat,:lng, presence: true
 end
