f922c2077460ff242fc94556c1faaefcbc27f1e9
Sarwan Kumar <srladuani@gmail.com>
2017-06-09 11:01:47 +0500

    display price on lead page

--- app/helpers/leads_helper.rb
+++ app/helpers/leads_helper.rb
@@ -1,2 +1,17 @@
 module LeadsHelper
+
+  def total_price lead
+    html = ""
+    if lead.coupon.nil?
+      html = <<-HTML
+        <strike> Rs. #{lead.quantity * lead.plant.price}</strike>
+        <span> Rs. #{lead.total_price(lead.quantity)}</span>
+      HTML
+    else
+      html = <<-HTML
+        <span> Rs. #{lead.total_price(lead.quantity)}</span>
+      HTML
+    end
+    html.html_safe
+  end
 end
