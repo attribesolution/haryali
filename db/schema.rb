4dae539ce672c57074c3f5b47dcc64d40caedc25
Sarwan Kumar <srladuani@gmail.com>
2017-06-20 17:38:12 +0500

    forms completed

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20170620042314) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "current", default: 0, null: false
-    t.integer "target", default: 1, null: false
+    t.integer "target", default: 0, null: false
   end
 
   create_table "plants", force: :cascade do |t|
