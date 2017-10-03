9d4172eb6cdb3ad3d71e02e3e6787598fc7efc41
sehrish <sehrishr2013@gmail.com>
2017-10-03 18:15:01 +0500

    creating lead for Yad

--- app/controllers/haryali_yaads_controller.rb
+++ app/controllers/haryali_yaads_controller.rb
@@ -4,12 +4,16 @@ class HaryaliYaadsController < ApplicationController
   end
 
   def submit_form
+    # code needs to be refactored
     lead = Lead.new
     lead.name = params[:name]
     lead.contact = params[:phone]
     lead.email = params[:email]
     lead.dedicate_name = params[:memory]
     lead.dedicate_type = 'memory'
+    lead.quantity = 1
+    lead.plant_id = Plant.first.id
+    lead.location_id = Location.first.id
     if lead.save
       if User.where(email: params[:email]).count == 0
         user = User.new(email: params[:email], password: 'password', password_confirmation: 'password', role: 0)
